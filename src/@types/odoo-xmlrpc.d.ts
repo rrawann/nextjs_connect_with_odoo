290f2ac6a9b7344a5e16bcfd68cff557c7b83164
Amal Saleh (ASH) <70725157+amalhq@users.noreply.github.com>
2024-10-30 14:15:31 +0300

    Update odoo-xmlrpc.d.ts

--- src/@types/odoo-xmlrpc.d.ts
+++ src/@types/odoo-xmlrpc.d.ts
@@ -2,6 +2,6 @@ declare module 'odoo-xmlrpc' {
     export class Client {
       constructor(options: { url: string; db: string; username: string; password: string });
       authenticate(): Promise<number>;
-      execute_kw(model: string, method: string, args: any[], kwargs?: any): Promise<any>;
+      execute_kw(model: string, method: string, args: any[], kwargs: any): Promise<any>;
     }
   }
